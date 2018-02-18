1a7f3a235678227cee4e97c33de616e91b52d887
Shrung Bhatt <shrungbhatt@gmail.com>
2018-02-18 14:51:53 +0530

    Added fonts and changed the login and sign up screen

---
+++ app/src/main/java/com/projects/shrungbhatt/photography/Activity_HomeScreen.java
@@ -0,0 +1,16 @@
+package com.projects.shrungbhatt.photography;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jigsaw on 18/2/18.
+ */
+
+public class Activity_HomeScreen extends AppCompatActivity {
+
+
+
+
+
+
+}

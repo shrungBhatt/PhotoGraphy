f0ac7dd8c84c812cd0953a41cc8920ab7c308608
Shrung Bhatt <shrungbhatt@gmail.com>
2018-02-18 11:13:50 +0530

    Created signup screen

---
+++ app/src/main/java/com/projects/shrungbhatt/photography/Activity_Login.java
@@ -0,0 +1,87 @@
+package com.projects.shrungbhatt.photography;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by jigsaw on 12/2/18.
+ */
+
+public class Activity_Login extends AppCompatActivity implements Validator.ValidationListener {
+
+
+    @NotEmpty
+    @Email
+    @BindView(R.id.user_email)
+    AutoCompleteTextView userEmail;
+    @NotEmpty
+    @Password(min = 6, scheme = Password.Scheme.ANY)
+    @BindView(R.id.user_password)
+    EditText userPassword;
+    @BindView(R.id.email_sign_in_button)
+    Button emailSignInButton;
+    @BindView(R.id.user_sign_up_button)
+    Button userSignUpButton;
+
+    Validator mValidator;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mValidator = new Validator(this);
+        mValidator.setValidationListener(this);
+    }
+
+    @OnClick({R.id.email_sign_in_button, R.id.user_sign_up_button})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.email_sign_in_button:
+                mValidator.validate();
+                break;
+            case R.id.user_sign_up_button:
+                break;
+        }
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(this, "Yay! we got it right!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
--- app/src/main/java/com/projects/shrungbhatt/photography/Activity_Signup.java
+++ app/src/main/java/com/projects/shrungbhatt/photography/Activity_Signup.java
@@ -1,13 +1,49 @@
 package com.projects.shrungbhatt.photography;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Activity_Signup extends AppCompatActivity {
+
+    @BindView(R.id.signup_user_name_edtTxt)
+    AutoCompleteTextView signupUserNameEdtTxt;
+    @BindView(R.id.signup_user_email_edtTxt)
+    AutoCompleteTextView signupUserEmailEdtTxt;
+    @BindView(R.id.signup_password_edtTxt)
+    AutoCompleteTextView signupPasswordEdtTxt;
+    @BindView(R.id.signup_retype_password_edtTxt)
+    AutoCompleteTextView signupRetypePasswordEdtTxt;
+    @BindView(R.id.signup_phone_no_edtTxt)
+    AutoCompleteTextView signupPhoneNoEdtTxt;
+    @BindView(R.id.signup_select_gender_spinner)
+    Spinner signupSelectGenderSpinner;
+    @BindView(R.id.signup_dob_button)
+    Button signupDobButton;
+    @BindView(R.id.signup_button)
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__signup);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.signup_dob_button, R.id.signup_button})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.signup_dob_button:
+                break;
+            case R.id.signup_button:
+                break;
+        }
     }
 }
